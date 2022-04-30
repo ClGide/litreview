76d020ba5f1598c89cd1b191b4871831bdafc9d7
ClGide <90548791+ClGide@users.noreply.github.com>
2022-04-30 17:41:06 +0200

    Update settings.py
    mini security modification

--- litreview/litreview/settings.py
+++ litreview/litreview/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%w#o^zo83v#si9!$pgsdr=9+ir5gce)v@uz5%_kma2uk*xss#r'
+# you stored the key in a .txt file 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
